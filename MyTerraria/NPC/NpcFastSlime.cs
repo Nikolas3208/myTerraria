0eaa352e494e0bd5650b7f61472ce5f9f29b93ce
Николай Гастелло <gastello.nik7803@mail.ru>
2021-01-01 17:33:29 +0300

    Added Project

---
+++ MyTerraria/NPC/NpcFastSlime.cs
@@ -0,0 +1,18 @@
+﻿using SFML.Graphics;
+using SFML.System;
+
+namespace MyTerraria.NPC
+{
+    class NpcFastSlime : NpcSlime
+    {
+        public NpcFastSlime(World world) : base(world)
+        {
+            rect.FillColor = new Color(255, 0, 0, 200);
+        }
+
+        public override Vector2f GetJumpVelocity()
+        {
+            return new Vector2f(Direction * World.Rand.Next(15, 100), -World.Rand.Next(8, 15));
+        }
+    }
+}
---
+++ MyTerraria/Program.cs
@@ -0,0 +1,89 @@
+﻿using SFML.Graphics;
+using SFML.System;
+using SFML.Window; 
+using System;
+
+namespace MyTerraria
+{
+    class Program
+    {
+        public static RenderWindow Window { private set; get; }
+        public static Game Game { private set; get; }
+        public static float Delta { private set; get; }
+
+        private static int horizontalShiftPx = Tile.TILE_SIZE;
+
+        static void Main(string[] args)
+        {
+            Window = new RenderWindow(new VideoMode(800, 600), "My Terraria!");
+            Window.SetVerticalSyncEnabled(true);
+
+            Window.Closed += Win_Closed;
+            Window.Resized += Win_Resized;
+            Window.KeyPressed += Win_KeyPressed;
+
+            // Загрузка контента
+            Content.Load();
+            
+            Game = new Game();      // Создаём новый объект класса игры
+            Clock clock = new Clock();
+
+            while (Window.IsOpen)
+            {
+                Delta = clock.Restart().AsSeconds();
+
+                Window.DispatchEvents();
+                
+                Game.Update();
+
+                Window.Clear(Color.Cyan);
+
+                Game.Draw();
+
+                Window.Display();
+            }
+        }
+
+        public static void Win_KeyPressed(object sender, KeyEventArgs e)
+        {
+            View view;
+
+            switch (e.Code)
+            {
+                case Keyboard.Key.D:
+                    view = Window.GetView();
+                    view.Move(new Vector2f(horizontalShiftPx, 0));
+                    Game.World.ChangeHorizontalShift(1);
+                    Window.SetView(view);
+                    break;
+                case Keyboard.Key.A:
+                    view = Window.GetView();
+                    view.Move(new Vector2f(-horizontalShiftPx, 0));
+                    Game.World.ChangeHorizontalShift(-1);
+                    Window.SetView(view);
+                    break;
+
+                case Keyboard.Key.Up:
+                    view = Window.GetView();
+                    view.Zoom(0.9f);
+                    Window.SetView(view);
+                    break;
+                case Keyboard.Key.Down:
+                    view = Window.GetView();
+                    view.Zoom(1.1f);
+                    Window.SetView(view);
+                    break;
+            }
+        }
+
+        private static void Win_Resized(object sender, SFML.Window.SizeEventArgs e)
+        {
+            Window.SetView(new View(new FloatRect(0, 0, e.Width, e.Height)));
+        }
+
+        private static void Win_Closed(object sender, EventArgs e)
+        {
+            Window.Close();
+        }
+    }
+}
